9469552a63889360710de916e77ed0b8b346df5b
Ubuntu <ubuntu@test.novalocal>
2016-10-30 22:17:51 +0000

    finished 2 more questions

--- firewallSetup.sh
+++ firewallSetup.sh
@@ -18,8 +18,12 @@
 #- Block connecting from your computer to any IP outside the range 130.15.0.0 to 130.15.255.255 [netmask /16]
 
 #- Only allow IP 130.15.100.100 to connect to your mysql running on port 3306
+# iptables -A INPUT -i eth0 -p tcp -s 130.15.100.100 --dport 3306 -m state --state NEW,ESTABLISHED -j ACCEPT
+# iptables -A OUTPUT -o eth0 -p tcp --sport 3306 -m state --state ESTABLISHED -j ACCEPT
 
 #- Drop outgoing ssh traffic to all other computers.
 
 #- Can you still ssh the VM from your host machine? Why? If you can’t, how to fix it?
+# Yes because all you have done is dropped all outgoing traffic from the server. You have not dropped incoming ssh connections.
+#
 
