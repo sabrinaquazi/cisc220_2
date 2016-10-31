7f347982c11fbda5eb3137bb1d8748f0371c8a31
Ubuntu <ubuntu@test.novalocal>
2016-10-31 03:54:28 +0000

    one question left

--- firewallSetup.sh
+++ firewallSetup.sh
@@ -14,6 +14,9 @@
 # iptables -A OUTPUT -o eth0 -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
 
 #- Drop any other incoming tcp traffic
+# still allowing ssh so I can access the virtual machine
+# sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+# sudo iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
 
 #- Block connecting from your computer to any IP outside the range 130.15.0.0 to 130.15.255.255 [netmask /16]
 # sudo iptables -A INPUT -s 130.15.0.0/16 -j DROP
@@ -23,6 +26,7 @@
 # iptables -A OUTPUT -o eth0 -p tcp --sport 3306 -m state --state ESTABLISHED -j ACCEPT
 
 #- Drop outgoing ssh traffic to all other computers.
+# sudo iptables -A OUTPUT -o eth0 -p tcp --dport 22 -j ACCEPT
 
 #- Can you still ssh the VM from your host machine? Why? If you can’t, how to fix it?
 # Yes because all you have done is dropped all outgoing traffic from the server. You have not dropped incoming ssh connections.
