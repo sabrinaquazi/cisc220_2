f3d5ff7b13b3b45d468f43b63a965f370f9b9f2b
Sabrina Quazi <sabrinaquazi@Sabrinas-MacBook-Air.local>
2016-10-31 18:06:19 -0400

    backed up files

--- repositoryBackup.sh
+++ repositoryBackup.sh
@@ -2,7 +2,7 @@
 #*1**1-7
 
 
-tar czvf backup$(date +%Y%m%d%H).tgz >> backupsLog
+tar czvf backup$(date +%Y%m%d%H).tgz $1
 
 git pull
 
@@ -10,7 +10,7 @@ git add .
 
 git commit -am 'backed up files' 
 
-git push https://$2:$3@$1 --all
+git push https://$3:$4@$2 --all
 
 echo $ "Backup backup$(date +%Y%m%d%H).tgz created sucessfully!"
 echo $ "Backup backup$(date +%Y%m%d%H).tgz committed to the local git repository"
