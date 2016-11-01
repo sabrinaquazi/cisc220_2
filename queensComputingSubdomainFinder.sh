e9b8a02e4dfdb58cf112026611834b537dea46b0
StefanSokic <stefan.sokic33@gmail.com>
2016-10-31 21:53:19 -0400

    finished the script

--- queensComputingSubdomainFinder.sh
+++ queensComputingSubdomainFinder.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-for ip in 130.15.{0..255}.{0..255}; do
-echo $ip
-nslookup $ip;
-# grep the output of nslookup to only extract the part after "name ="
+# print the header
+echo -e 'IP Address' '\t' 'Sub-Domain';
+# loop through all ip's in the desired range
+for ip in 130.15.{0..255}.{0..255}; do  
+# use nslookup and print the substring whereever there is an ip avaliable
+DOMAIN=$( nslookup $ip | awk '/name =/{print substr($4,1)}')
+# print all the results
+echo -e $ip '\t' $DOMAIN
 done
